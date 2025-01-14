5397176e79f6e2cf7c18e84bd5ebbd6bc79f916e
Vinay Ram Gazula <vinayramgazula@gmail.com>
2025-01-13 21:57:07 -0500

    using JS for project href and resources links;

--- js/main.js
+++ js/main.js
@@ -28,9 +28,9 @@
 
 	var partialHeight = function() {
 
-		$('.js-partialheight').css('height', $(window).height() * 0.333333);
+		$('.js-partialheight').css('height', $(window).height() * 0.5);
 		$(window).resize(function(){
-			$('.js-partialheight').css('height', $(window).height() * 0.333333);
+			$('.js-partialheight').css('height', $(window).height() * 0.5);
 		});
 
 	};
@@ -48,19 +48,172 @@
 		2: "WebScrapping-R",
 		1: "UnstructuredFVM"
 	};
-	
+
+	const projectsMap = {
+		10: {
+			title: "TradeForecast",
+			src: "project10.html",
+			githubRepo: "vinay-ram1999/TradeForecast",
+			reportLink: "https://linktoreport.com/tradeforecast",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		9: {
+			title: "Healthcare Analytics",
+			src: "project9.html",
+			githubRepo: "vinay-ram1999/Healthcare-Analytics",
+			reportLink: "https://linktoreport.com/healthcareanalytics",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		8: {
+			title: "SRDB Analytics",
+			src: "project8.html",
+			githubRepo: "vinay-ram1999/SRDBv5-Analytics",
+			reportLink: "https://linktoreport.com/srdbanalytics",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		7: {
+			title: "Event-Driven Alerts",
+			src: "project7.html",
+			githubRepo: "vinay-ram1999/user_management",
+			reportLink: "https://linktoreport.com/eventdrivenalerts",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		6: {
+			title: "UniversityRankings",
+			src: "project6.html",
+			reportLink: "https://linktoreport.com/universityrankings",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		5: {
+			title: "SolarFlareNet xAI",
+			src: "project5.html",
+			githubRepo: "vinay-ram1999/SolarFlareNet",
+			paperLink: "https://linktopublication.com/solarflarenetxai", // Example placeholder link
+			reportLink: "https://linktoreport.com/solarflarenetxai",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		4: {
+			title: "AlgoTrade-API",
+			src: "project4.html",
+			githubRepo: "vinay-ram1999/AlgoTrade-API",
+			reportLink: "https://linktoreport.com/algotradeapi",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		3: {
+			title: "eStore DB Design",
+			src: "project3.html",
+			githubRepo: "vinay-ram1999/CS631-DMSD",
+			reportLink: "https://linktoreport.com/estoredbdesign",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		2: {
+			title: "WebScrapping-R",
+			src: "project2.html",
+			githubRepo: "vinay-ram1999/WebScraping-R",
+			reportLink: "https://linktoreport.com/webscrappingr",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		},
+		1: {
+			title: "UnstructuredFVM",
+			src: "project1.html",
+			githubRepo: "UnstructuredFVM",
+			paperLink: "https://linktopublication.com/unstructuredfvm", // Example placeholder link
+			reportLink: "https://linktoreport.com/unstructuredfvm",
+			techStack: ["Python", "SQL", "PyTorch", "Tableau"]
+		}
+	};
+
+	// Function to set project html source href
+	var assignProjectSrc = function() {
+		const projectSrcElements = document.querySelectorAll('.project-src');
+		projectSrcElements.forEach(element => {
+			const projectNumber = element.getAttribute('project-number');
+			const projectData = projectsMap[projectNumber];
+			element.href = projectData.src;
+		});
+	}
+	assignProjectSrc();
+
 	// Function to set project titles dynamically based on attribute
 	var assignProjectTitles = function() {
-	const projectElements = document.querySelectorAll('.project-title');
-	projectElements.forEach(element => {
-		const projectNumber = element.getAttribute('project-number');
-		const title = projectTitlesMap[projectNumber]; // Retrieve title from map
-		if (title) {
-		  element.innerText = title; // Set the project title
-		} 
-	  	});
+		const projectElements = document.querySelectorAll('.project-title');
+		projectElements.forEach(element => {
+			const projectNumber = element.getAttribute('project-number');
+			const projectData = projectsMap[projectNumber];
+			element.innerText = projectData.title; // Set the project title
+		});
 	}
 	assignProjectTitles();
+
+	// Function to set project tech stack dynamically based on attribute
+	var assignProjectTechStack = function() {
+		const stackElements = document.querySelectorAll('.project-stack');
+		stackElements.forEach(element => {
+			const projectNumber = element.getAttribute('project-number');
+			const projectData = projectsMap[projectNumber];
+			const techStack = projectData.techStack;
+			if (techStack) {
+				techStack.forEach(tech => {
+				const techTag = document.createElement('a');
+				techTag.className = 'tag-cloud-link';
+				techTag.innerText = tech;
+				element.appendChild(techTag);
+			  });
+			}
+		});
+	}
+	assignProjectTechStack();
+
+	// Function to set github hrefs
+	var assignGitRefs = function() {
+		const githubElements = document.querySelectorAll('.github-repo');
+		githubElements.forEach(element => {
+			const projectNumber = element.getAttribute('project-number');
+			const githubBadge = element.getAttribute('repo-badges');
+			const projectData = projectsMap[projectNumber];
+			const githubRepo = projectData.githubRepo;
+			if (githubRepo) {
+				element.href = `https://github.com/${githubRepo}`;
+				element.target = '_blank';
+				element.rel = 'noopener noreferrer';
+				if (githubBadge) {
+					element.innerHTML = `<img class="github-stars" src="https://img.shields.io/github/stars/${githubRepo}"> <img class="github-forks" src="https://img.shields.io/github/forks/${githubRepo}">`;
+				}
+			}
+		});
+	}
+	assignGitRefs();
+
+	// Function to set project sidebar links
+	var assignProjectLinks = function() {
+		const sidebarSocialElements = document.querySelectorAll('.project-sidebar-links');
+		sidebarSocialElements.forEach(element => {
+			const projectNumber = element.getAttribute('project-number');
+			const projectData = projectsMap[projectNumber];
+			const githubLink = element.querySelector('.github-link');
+			const reportLink = element.querySelector('.report-link');
+			const paperLink = element.querySelector('.paper-link');
+			if (githubLink && projectData.githubRepo) {
+				githubLink.href = `https://github.com/${projectData.githubRepo}`;
+				githubLink.target = '_blank';
+				githubLink.rel = 'noopener noreferrer';
+				githubLink.innerHTML = '<span class="icon-github"></span>GitHub Repository';
+			}
+			if (reportLink && projectData.reportLink) {
+				reportLink.href = projectData.reportLink;
+				reportLink.target = '_blank';
+				reportLink.rel = 'noopener noreferrer';
+				reportLink.innerHTML = '<span class="fa fa-file-pdf-o" aria-hidden="true"></span>Project Report';
+			}
+			if (paperLink && projectData.paperLink) {
+				paperLink.href = projectData.paperLink;
+				paperLink.target = '_blank';
+				paperLink.rel = 'noopener noreferrer';
+				paperLink.innerHTML = '<span class="fas fa-file-alt"></span>Publication';
+			}
+		});
+	}
+	assignProjectLinks();
 
 	const creditsHTML = `Copyright &copy; ${new Date().getFullYear()} All rights reserved | Made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>`;
 
